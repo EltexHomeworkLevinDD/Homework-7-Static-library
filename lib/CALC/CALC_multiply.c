69c637bad1f2880ffeb1e3bdb9944f517710a406
EltexHomeworkLevinDD <pykanus172@gmail.com>
2024-01-08 22:28:09 +0700

    Tasks

--- lib/CALC/CALC_multiply.c
+++
@@ -1,6 +0,0 @@
-#include "CALC.h"
-
-// Multiply numbers (value1*value2)
-int CALC_multiply(int value1, int value2){
-    return value1 * value2;
-}
--- lib/CALC/CALC_substract.c
+++
@@ -1,6 +0,0 @@
-#include "CALC.h"
-
-// Sum numbers (value1-value2)
-int CALC_substract(int value1, int value2){
-    return value1 - value2;
-}
